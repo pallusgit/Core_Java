8c0acf4fa38a6fe2d58750986ba4b0134fa468a6
Chintu-Pallu <Chintu-Pallu@DESKTOP-LCNGNO5>
2022-05-27 17:57:23 +0530

    commit

---
+++ HalfPyramid/src/HalfPyramid.java
@@ -0,0 +1,16 @@
+
+public class HalfPyramid {
+
+	public static void main(String[] args) {
+		int rowa = 5;
+		for (int i = 1; i <= rows; ++i) {
+			for (int j = 1; j <= i; ++j) {
+				System.out.print(" ");
+			}
+		System.out.println();
+		}
+		// TODO Auto-generated method stub
+
+	}
+
+}
