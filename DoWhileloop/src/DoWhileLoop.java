1f054b5f063886e13bb295915de8eebfe7977478
ADMIN <ADMIN@DESKTOP-EVH9NRA>
2022-05-31 12:43:13 +0530

    first commit

---
+++ DoWhileloop/src/DoWhileLoop.java
@@ -0,0 +1,15 @@
+
+public class DoWhileLoop {
+
+	public static void main(String[] args) {
+		int i=1;    
+	    do{    
+	        System.out.println(i);    
+	    i++;    
+	    }while(i<=10);
+				}
+		// TODO Auto-generated method stub
+
+	}
+
+
