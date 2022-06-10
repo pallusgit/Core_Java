b6597f3e45ba7389504917a94e2b25d8ea22ee5d
Chintu-Pallu <Chintu-Pallu@DESKTOP-LCNGNO5>
2022-06-10 21:18:25 +0530

    First Commit

---
+++ Ifdemo/src/IfDemo.java
@@ -0,0 +1,17 @@
+
+public class IfDemo {
+	{
+		static void withdraw()
+		{
+			System.out.println("Money is withdrawn");
+		}
+		public static void main(String[] args)
+		{
+			int balance=100;
+			if(balance>500)
+				ifDemo.withdraw();
+				System.out.println("Simple if demonstration");
+		}
+	}
+
+}
